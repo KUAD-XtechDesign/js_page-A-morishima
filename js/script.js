ecb623b9dc0ffc32c1ba3862432defd8529b4706
A-morishima <a-morishima3277@st.kyoto-art.ac.jp>
2021-10-29 15:27:26 +0900

    編集

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-$(function(){
+const CLASSNAME = "-visible";
+const TIMEOUT = 1500;
+const $target = $(".title");
 
-  
-})
+setInterval(() => {
+  $target.addClass(CLASSNAME);
+  setTimeout(() => {
+    $target.removeClass(CLASSNAME);
+  }, TIMEOUT);
+}, TIMEOUT * 2);
+
+
+Resourcesß
